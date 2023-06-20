c338d6a17898a24d9030fde27eab61c11aa990e2
DinnyTech <77016654+Danobentey@users.noreply.github.com>
2023-06-20 21:58:23 +0100

    feat: make dashboard cards clickable

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,22 +1,26 @@
 import React from "react";
-import { Card, CardBody } from "reactstrap"
+import { NavLink } from "react-router-dom";
+import { Card, CardBody } from "reactstrap";
 
 interface CardProps {
   imgSrc: string;
   title: string;
   value: number;
+  link?: string;
 }
 
-const Cards :React.FC<CardProps> = ({imgSrc, title, value}) => {
+const Cards: React.FC<CardProps> = ({ imgSrc, title, value, link }) => {
   return (
     <Card className="dashboard-card" fluid>
-      <CardBody>
-        <img src={imgSrc} alt="" />
-        <p>{title}</p>
-        <b>{value}</b>
-      </CardBody>
+      <NavLink to={link || ''}>
+        <CardBody>
+          <img src={imgSrc} alt="" />
+          <p>{title}</p>
+          <b>{value}</b>
+        </CardBody>
+      </NavLink>
     </Card>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -2,13 +2,14 @@ import { Col, Container, Row } from "reactstrap";
 import { DASH_IMAGES } from "../../assets/img/dashboard";
 import Cards from "../Cards";
 import "./dashboard.scss";
+import { pageRoutes } from "../../variables/pageRoutes";
 
 const Dashboard = () => {
   return (
     <Container className="dashboard">
       <Row className="p-0">
         <Col p-0 lg="3" sm="6" xs="12" >
-          <Cards imgSrc={DASH_IMAGES.users} title="USERS" value={1234} />
+          <Cards imgSrc={DASH_IMAGES.users} title="USERS" value={1234} link={pageRoutes.ALL_USERS} />
         </Col>
         <Col lg="3" sm="6" xs="12" >
           <Cards imgSrc={DASH_IMAGES.activeUsers} title="ACTIVE USERS" value={1234} />
